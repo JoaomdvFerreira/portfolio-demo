92e9ca1a0da61e175ae17f6e300473460dd56b27
joao.ferreira <joao.ferreira@savills.com>
2023-01-17 06:42:44 +0000

    adding google tag manager to the project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import TagManager, { TagManagerArgs } from 'react-gtm-module';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID || '';
+  const tagManagerArgs: TagManagerArgs = {
+    gtmId,
+  };
+
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
