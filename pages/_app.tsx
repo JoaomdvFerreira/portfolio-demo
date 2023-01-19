21994b1b51ee9d6e39f9d1b2129680c5a6f42b04
joao.ferreira <joao.ferreira@savills.com>
2023-01-19 20:55:36 +0000

    testing bradgarropy google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,26 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import TagManager, { TagManagerArgs } from 'react-gtm-module';
-import { useEffect } from 'react';
+// import TagManager, { TagManagerArgs } from 'react-gtm-module';
+// import { useEffect } from 'react';
+
+import GoogleAnalytics from '@bradgarropy/next-google-analytics';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const gtmId = process.env.NEXT_PUBLIC_GTM_ID || '';
-  const tagManagerArgs: TagManagerArgs = {
-    gtmId,
-  };
+  // const gtmId = process.env.NEXT_PUBLIC_GTM_ID || '';
+  // const tagManagerArgs: TagManagerArgs = {
+  //   gtmId,
+  // };
 
-  useEffect(() => {
-    TagManager.initialize(tagManagerArgs);
-  }, []);
+  // useEffect(() => {
+  //   TagManager.initialize(tagManagerArgs);
+  // }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GoogleAnalytics measurementId={`${process.env.NEXT_PUBLIC_GTM_ID}`} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
